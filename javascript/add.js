90325562b0301d12e1b82fbae92890fbdc020064
ZahraAlqaisi <>
2021-05-05 12:15:46 +0000

    push js

---
+++ javascript/add.js
@@ -0,0 +1,24 @@
+var userName ='zahra';
+
+alert ('hi');
+
+alert (userName);
+
+alert ('enjoy our albums');
+
+
+
+
+document.write('<h3>'+userName+'</h3>')
+
+var opinion=prompt('do you like our website?');
+
+console.log(opinion);
+
+
+
+if(opinion =='yes'){
+document.write('<img src="https://ideas.hallmark.com/wp-content/uploads/2020/08/WTW_ThankYouCards.jpg" width=250px">');
+}else if(opinion==='no'){
+  document.write('<img src="https://image.shutterstock.com/image-vector/thank-you-visiting-us-vector-260nw-1214737042.jpg" width=250px"');
+}
